c82ca64a453b27958e242930a8ce24a1cf587d6f
biezhi <biezhi.me@gmail.com>
2017-07-10 21:59:50 +0800

    init project

--- src/main/java/com/blade/validator/annotation/Valid.java
+++ src/main/java/com/blade/validator/annotation/Valid.java
@@ -14,7 +14,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.blade.validator;
+package com.blade.validator.annotation;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
